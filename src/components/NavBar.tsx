5fd4da6a75a3369cfba9006dbf0b00b5e61bf755
apolorezende <apolo_henriquerr@hotmail.com>
2023-12-01 16:32:58 -0300

    feat:navbar condition add

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useRouter } from 'next/router';
 
 export function NavBar() {
+  const router = useRouter();
   return (
     <>
       <div className="fixed w-full top-2 px-20 bg-white flex items-center">
@@ -12,11 +14,30 @@ export function NavBar() {
           height={58}
           alt="Logo com as iniciais H e V escritos"
         />
-        <div className="flex justify-around w-full">
-          <Link href={"/sobre"}>Sobre mim</Link>
-          <Link href={"#projetos"}>Projetos</Link>
-          <Link href={"#contato"}>Contato</Link>
-        </div>
+        {router.pathname === '/' ? (
+          <div className="flex justify-around w-full">
+            <Link href={"/sobre"}>Sobre mim</Link>
+            <Link href={"#projetos"}>Projetos</Link>
+            <Link href={"#contato"}>Contato</Link>
+          </div>
+        ) : (
+          <div className="flex justify-around w-full" >
+            <Link href={"/"} className="flex flex-row">
+              <Image
+                src="house.svg"
+                width={19}
+                height={20}
+                alt="Icon do botão Home"
+              />
+              voltar para o inicio
+            </Link>
+            <div/>
+            <div/>
+            <div/>
+          </div>
+
+        )}
+        
       </div>
     </>
   );
